77d10eb6069694b7178fc9b6b30fa8f35cf65759
Eric Chang <pkerichang@gmail.com>
2018-02-14 15:53:04 -0800

    remove substrate parity sweep.

--- scripts_test/diffamp.py
+++ scripts_test/diffamp.py
@@ -28,30 +28,27 @@ def generate(prj, specs, gen_sch=True, run_lvs=False):
     params = specs['params']
     lch_list = specs['swp_params']['lch']
     gr_nf_list = specs['swp_params']['guard_ring_nf']
-    sub_par_list = specs['swp_params'].get('sub_parity', [0])
 
     temp_db = make_tdb(prj, impl_lib, specs)
 
     temp_list = []
     name_list = []
-    name_fmt = 'DIFFAMP_L%s_gr%d_spar%d'
+    name_fmt = 'DIFFAMP_L%s_gr%d'
     for gr_nf in gr_nf_list:
         for lch in lch_list:
-            for sub_par in sub_par_list:
-                cur_name = name_fmt % (float_to_si_string(lch), gr_nf, sub_par)
+            cur_name = name_fmt % (float_to_si_string(lch), gr_nf)
 
-                params['lch'] = lch
-                params['guard_ring_nf'] = gr_nf
-                params['options']['sub_parity'] = sub_par
-                temp = temp_db.new_template(params=params, temp_cls=DiffAmp, debug=False)
-                temp_list.append(temp)
-                name_list.append(cur_name)
+            params['lch'] = lch
+            params['guard_ring_nf'] = gr_nf
+            temp = temp_db.new_template(params=params, temp_cls=DiffAmp, debug=False)
+            temp_list.append(temp)
+            name_list.append(cur_name)
 
-                if gen_sch:
-                    dsn = prj.create_design_module(lib_name=sch_lib, cell_name=sch_cell)
-                    dsn.design(**temp.sch_params)
-                    print('creating schematic for %s' % cur_name)
-                    dsn.implement_design(impl_lib, top_cell_name=cur_name)
+            if gen_sch:
+                dsn = prj.create_design_module(lib_name=sch_lib, cell_name=sch_cell)
+                dsn.design(**temp.sch_params)
+                print('creating schematic for %s' % cur_name)
+                dsn.implement_design(impl_lib, top_cell_name=cur_name)
 
     print('creating layout')
     temp_db.batch_layout(prj, temp_list, name_list)
