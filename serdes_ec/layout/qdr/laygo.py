242a03081dc8df6354d35fb952a094af8e0c54d5
Eric Chang <pkerichang@gmail.com>
2018-03-08 08:14:23 -0800

    working on Sinusoidal clock divider.

--- serdes_ec/layout/qdr/laygo.py
+++ serdes_ec/layout/qdr/laygo.py
@@ -6,6 +6,8 @@ from typing import TYPE_CHECKING, Dict, Any, Set
 
 from abs_templates_ec.laygo.core import LaygoBase
 
+from bag.layout.routing import TrackManager
+
 if TYPE_CHECKING:
     from bag.layout.template import TemplateDB
 
@@ -44,20 +46,33 @@ class SinClkDivider(LaygoBase):
         return dict(
             config='laygo configuration dictionary.',
             row_layout_info='The AnalogBase layout information dictionary.',
-            seg_dict='Number of segments dictionary.'
+            seg_dict='Number of segments dictionary.',
+            tr_widths='Track width dictionary.',
+            tr_spaces='Track spacing dictionary.',
         )
 
     def draw_layout(self):
         row_layout_info = self.params['row_layout_info']
         seg_dict = self.params['seg_dict'].copy()
+        tr_widths = self.params['tr_widths']
+        tr_spaces = self.params['tr_spaces']
+
+        tr_manager = TrackManager(self.grid, tr_widths, tr_spaces, half_space=True)
 
         seg_sr = self._get_sr_latch_info(seg_dict)
+        num_col = seg_sr
 
-        self.set_rows_direct(row_layout_info, num_col=seg_sr)
+        self.set_rows_direct(row_layout_info, num_col=num_col)
 
-        self._draw_sr_latch(0, seg_sr, seg_dict)
+        vss_ports, vdd_ports = self._draw_substrate(num_col)
+        self._draw_sr_latch(num_col - seg_sr, num_col, seg_dict, tr_manager)
 
         self.fill_space()
+
+    def _draw_substrate(self, num_col):
+        nsub = self.add_laygo_mos(0, 0, num_col)
+        psub = self.add_laygo_mos(self.num_rows - 1, 0, num_col)
+        return nsub, psub
 
     @classmethod
     def _get_sr_latch_info(cls, seg_dict):
@@ -70,47 +85,57 @@ class SinClkDivider(LaygoBase):
 
         if seg_inv % 2 != 0 or seg_drv % 2 != 0 or seg_sp % 2 != 0:
             raise ValueError('This generator only works for even sr_inv/sr_drv/sr_sp.')
-        if seg_set < seg_nand * 2:
-            raise ValueError('This generator only works if seg_set >= seg_nand * 2')
+        if seg_sp < 2:
+            raise ValueError('sr_sp must be >= 2.')
 
-        return (seg_inv + seg_drv + seg_sp + seg_set) * 2
+        seg_nand_set = max(seg_nand * 2, seg_set)
+        return (seg_inv + seg_drv + seg_sp + seg_nand_set) * 2
 
-    def _draw_sr_latch(self, start, seg_tot, seg_dict):
+    def _draw_sr_latch(self, start, seg_tot, seg_dict, tr_manager):
         seg_nand = seg_dict['sr_nand']
         seg_set = seg_dict['sr_set']
         seg_sp = seg_dict['sr_sp']
         seg_inv = seg_dict['sr_inv']
         seg_drv = seg_dict['sr_drv']
+        seg_nand_set = max(seg_nand * 2, seg_set)
 
+        # place instances
         stop = start + seg_tot
         ridx = 2
-        nx = seg_set // 2
-        setl = self.add_laygo_primitive('fg2d', loc=(start, ridx), nx=nx, spx=2)
-        setr = self.add_laygo_primitive('fg2d', loc=(stop - seg_set, ridx), nx=nx, spx=2)
-        delta = seg_set - seg_nand * 2
-        start += delta
-        stop += delta
-
+        setl = self.add_laygo_mos(ridx, start + seg_nand_set - seg_set, seg_set)
+        setr = self.add_laygo_mos(ridx, stop - seg_nand_set, seg_set)
         ridx += 1
-        nx = seg_nand
-        nnandl = self.add_laygo_primitive('stack2s', loc=(start, ridx), nx=nx, spx=2)
-        nnandr = self.add_laygo_primitive('stack2s', loc=(stop - seg_nand * 2, ridx),
-                                          nx=nx, spx=2)
-        pnandl = self.add_laygo_primitive('stack2s', loc=(start, ridx + 1), nx=nx, spx=2)
-        pnandr = self.add_laygo_primitive('stack2s', loc=(stop - seg_nand * 2, ridx + 1),
-                                          nx=nx, spx=2)
-        delta = seg_nand * 2 + seg_sp
-        start += delta
-        stop -= delta
-        nx = seg_drv // 2
-        ndrvl = self.add_laygo_primitive('fg2d', loc=(start, ridx), nx=nx, spx=2)
-        ndrvr = self.add_laygo_primitive('fg2d', loc=(stop - seg_drv, ridx), nx=nx, spx=2)
-        pdrvl = self.add_laygo_primitive('fg2d', loc=(start, ridx + 1), nx=nx, spx=2)
-        pdrvr = self.add_laygo_primitive('fg2d', loc=(stop - seg_drv, ridx + 1), nx=nx, spx=2)
+        cidx = start + seg_nand_set - seg_nand * 2
+        nnandl = self.add_laygo_mos(ridx, cidx, seg_nand, gate_loc='s', stack=True)
+        pnandl = self.add_laygo_mos(ridx + 1, cidx, seg_nand, gate_loc='s', stack=True)
+        cidx = stop - seg_nand_set
+        nnandr = self.add_laygo_mos(ridx, cidx, seg_nand, gate_loc='s', stack=True, flip=True)
+        pnandr = self.add_laygo_mos(ridx + 1, cidx, seg_nand, gate_loc='s', stack=True, flip=True)
+        start += seg_nand_set + seg_sp
+        stop -= seg_nand_set + seg_sp
+
+        ndrvl = self.add_laygo_mos(ridx, start, seg_drv)
+        pdrvl = self.add_laygo_mos(ridx + 1, start, seg_drv)
+        cidx = stop - seg_drv
+        ndrvr = self.add_laygo_mos(ridx, cidx, seg_drv)
+        pdrvr = self.add_laygo_mos(ridx + 1, cidx, seg_drv)
         start += seg_drv
         stop -= seg_drv
-        nx = seg_inv // 2
-        ninvl = self.add_laygo_primitive('fg2d', loc=(start, ridx), nx=nx, spx=2)
-        ninvr = self.add_laygo_primitive('fg2d', loc=(stop - seg_inv, ridx), nx=nx, spx=2)
-        pinvl = self.add_laygo_primitive('fg2d', loc=(start, ridx + 1), nx=nx, spx=2)
-        pinvr = self.add_laygo_primitive('fg2d', loc=(stop - seg_inv, ridx + 1), nx=nx, spx=2)
+
+        ninvl = self.add_laygo_mos(ridx, start, seg_inv)
+        pinvl = self.add_laygo_mos(ridx + 1, start, seg_inv)
+        cidx = stop - seg_inv
+        ninvr = self.add_laygo_mos(ridx, cidx, seg_inv)
+        pinvr = self.add_laygo_mos(ridx + 1, cidx, seg_inv)
+
+        # compute track locations
+
+        # gather wires
+        vss_list = [inst['s'] for inst in (setl, setr, ndrvl, ndrvr, ninvl, ninvr)]
+        vdd_list = [inst['s'] for inst in (pdrvl, pdrvr, pinvl, pinvr)]
+        q_list = self.connect_wires([inst['d'] for inst in (nnandl, pnandl, ndrvl, pdrvl)])
+        qb_list = self.connect_wires([inst['d'] for inst in (nnandr, pnandr, ndrvr, pdrvr)])
+        s_list = self.connect_wires([ninvl['d'], pinvl['d']])
+        r_list = self.connect_wires([ninvr['d'], pinvr['d']])
+        nand_vss_list = [nnandl['s'], nnandr['s']]
+        nand_vdd_list = [pnandl['s'], pnandr['s']]
