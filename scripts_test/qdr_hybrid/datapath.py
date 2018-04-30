724a3d610045998e9a099ad2daefc325c88afd2b
Eric Chang <pkerichang@gmail.com>
2018-04-29 23:10:39 -0700

    update RX datapath test script.

--- scripts_test/qdr_hybrid/datapath.py
+++ scripts_test/qdr_hybrid/datapath.py
@@ -21,4 +21,4 @@ if __name__ == '__main__':
         bprj = local_dict['bprj']
 
     bprj.generate_cell(block_specs, RXDatapath, debug=True)
-    # bprj.generate_cell(block_specs, RXDatapath, gen_sch=True, run_lvs=True)
+    # bprj.generate_cell(block_specs, RXDatapath, gen_sch=True, run_lvs=False, debug=True)
