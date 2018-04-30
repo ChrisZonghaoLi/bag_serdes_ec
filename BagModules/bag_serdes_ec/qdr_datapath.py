32f9dd0fe52c49bf5b959f594b027b56af05ee96
Eric Chang <pkerichang@gmail.com>
2018-04-29 23:10:13 -0700

    updated RX datapath schematic generator to connect the pins properly.

--- BagModules/bag_serdes_ec/qdr_datapath.py
+++ BagModules/bag_serdes_ec/qdr_datapath.py
@@ -39,3 +39,33 @@ class bag_serdes_ec__qdr_datapath(Module):
         self.instances['XTAP1'].design(**tap1_params)
         self.instances['XOFFL'].design(**loff_params)
         self.instances['XSAMP'].design(**samp_params)
+
+        has_set = False
+        has_ffe = False
+        tapx_pins = self.instances['XTAPX'].master.pin_list
+        for name in tapx_pins:
+            if name.startswith('casc'):
+                has_ffe = True
+                new_name = 'bias_ffe' + name[4:]
+                self.rename_pin('bias_ffe<7:4>', new_name)
+                self.reconnect_instance_terminal('XTAPX', name, new_name)
+            elif name.startswith('bias_s'):
+                suffix = name[6:]
+                max_idx = int(suffix[1:].split(':')[0])
+                sgnp_name = 'sgnp_dfe' + suffix
+                sgnn_name = 'sgnn_dfe' + suffix
+                self.rename_pin('sgnp_dfe<11:8>', sgnp_name)
+                self.rename_pin('sgnn_dfe<11:8>', sgnn_name)
+                self.rename_pin('clk_dfe<11:4>', 'clk_dfe<%d:4>' % max_idx)
+                self.reconnect_instance_terminal('XTAPX', 'sgnp' + suffix, sgnp_name)
+                self.reconnect_instance_terminal('XTAPX', 'sgnn' + suffix, sgnn_name)
+                self.reconnect_instance_terminal('XTAPX', name, 'clk_dfe<%d:8>' % max_idx)
+            elif name.startswith('setp'):
+                has_set = True
+                # TODO: figure out the rest
+
+        if not has_ffe:
+            self.remove_pin('bias_ffe<7:4>')
+        if not has_set:
+            self.remove_pin('setp')
+            self.remove_pin('setn')
