ac2d15d238c29efd2bec3350e3c54ce757032332
Eric Chang <pkerichang@gmail.com>
2018-05-04 01:50:43 -0700

    1. update sampler column generation script.

--- scripts_test/qdr_hybrid/sampler_column.py
+++ scripts_test/qdr_hybrid/sampler_column.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import yaml
 
 from bag.core import BagProject
@@ -9,25 +11,35 @@ from serdes_ec.layout.qdr_hybrid.sampler import SamplerColumn
 
 
 def run_main(prj):
-    with open('specs_test/qdr_hybrid/sampler_column.yaml', 'r') as f:
+    root_name = 'specs_test/serdes_ec/qdr_hybrid'
+    test_fname = os.path.join(root_name, 'sampler_column_info.yaml')
+    if not os.path.isfile(test_fname):
+        with open(os.path.join(root_name, 'tap1_summer.yaml'), 'r') as f:
+            sum_specs = yaml.load(f)
+
+        impl_lib = sum_specs['impl_lib']
+        grid_specs = sum_specs['routing_grid']
+
+        tdb = prj.make_template_db(impl_lib, grid_specs)
+        summer = tdb.new_template(params=sum_specs['params'], temp_cls=Tap1Summer)
+        sampler_info = dict(sum_row_info=summer.sum_row_info,
+                            lat_row_info=summer.lat_row_info,
+                            div_tr_info=summer.div_tr_info,
+                            sup_tids=summer.params['sup_tids'])
+
+        with open(test_fname, 'w') as f:
+            yaml.dump(sampler_info, f)
+    else:
+        with open(test_fname, 'r') as f:
+            sampler_info = yaml.load(f)
+
+    with open(os.path.join(root_name, 'sampler_column.yaml'), 'r') as f:
         samp_specs = yaml.load(f)
-    with open('specs_test/qdr_hybrid/tap1_summer.yaml', 'r') as f:
-        sum_specs = yaml.load(f)
 
-    impl_lib = samp_specs['impl_lib']
-    grid_specs = samp_specs['routing_grid']
+    samp_specs['params'].update(sampler_info)
 
-    tdb = prj.make_template_db(impl_lib, grid_specs)
-    summer = tdb.new_template(params=sum_specs['params'], temp_cls=Tap1Summer)
-
-    params = samp_specs['params']
-    params['sum_row_info'] = summer.sum_row_info
-    params['lat_row_info'] = summer.lat_row_info
-    params['div_tr_info'] = summer.div_tr_info
-    params['sup_tids'] = summer.params['sup_tids']
-
-    # prj.generate_cell(samp_specs, SamplerColumn, debug=True)
-    prj.generate_cell(samp_specs, SamplerColumn, gen_sch=True, run_lvs=False, debug=True)
+    prj.generate_cell(samp_specs, SamplerColumn, debug=True)
+    # prj.generate_cell(samp_specs, SamplerColumn, gen_sch=True, run_lvs=False, debug=True)
 
 
 if __name__ == '__main__':
