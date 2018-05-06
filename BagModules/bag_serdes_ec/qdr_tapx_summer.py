6dd43a1120b936324a53ebe9eb2073938334982e
Eric Chang <pkerichang@gmail.com>
2018-05-06 09:24:09 -0700

    1. update tapx schematic generators.

--- BagModules/bag_serdes_ec/qdr_tapx_summer.py
+++ BagModules/bag_serdes_ec/qdr_tapx_summer.py
@@ -50,18 +50,36 @@ class bag_serdes_ec__qdr_tapx_summer(Module):
         for base_name in ('inp_a', 'inn_a', 'outp_a', 'outn_a'):
             self.rename_pin(base_name, base_name + a_arr)
         # compute term list
-        term_list, name_list = [], []
+        load_idx = 0
+        term_list = []
+        name_list = []
+        sum_params_list = []
+        load_params_list = []
         for fidx in range(num_ffe - 1, -1, -1):
-            term_dict = dict(inp='inp_a<%d>' % fidx, inn='inn_a<%d>' % fidx,
-                             outp_l='outp_a<%d>' % fidx, outn_l='outn_a<%d>' % fidx)
+            ffe_params = ffe_params_list[fidx]
+            ffe_suf = '<%d>' % fidx
+            load_suf = '<%d>' % load_idx
+            load_idx += 1
+            if ffe_params.get('flip_sign', False):
+                outp_s = 'iin' + load_suf
+                outn_s = 'iip' + load_suf
+            else:
+                outp_s = 'iip' + load_suf
+                outn_s = 'iin' + load_suf
+            term_dict = dict(inp='inp_a' + ffe_suf, inn='inn_a' + ffe_suf,
+                             outp_l='outp_a' + ffe_suf, outn_l='outn_a' + ffe_suf,
+                             outp_s=outp_s, outn_s=outn_s)
             if fidx != 0:
-                term_dict['casc'] = 'casc<%d>' % fidx
+                term_dict['casc'] = 'casc' + ffe_suf
             term_list.append(term_dict)
             name_list.append('XFFE%d' % fidx)
+            sum_params_list.append(ffe_params['sum_params'])
+            load_params_list.append(ffe_params['load_params'])
+
         # array instance, and design
         self.array_instance('XFFE', name_list, term_list=term_list)
-        for idx, ffe_params in enumerate(ffe_params_list):
-            self.instances['XFFE'][num_ffe - 1 - idx].design(**ffe_params)
+        for idx, sum_params in enumerate(sum_params_list):
+            self.instances['XFFE'][idx].design(**sum_params)
 
         # design DFE
         # rename/remove pins/instances
@@ -83,26 +101,54 @@ class bag_serdes_ec__qdr_tapx_summer(Module):
 
         if num_dfe > 0:
             # compute term list
-            term_list, name_list = [], []
+            term_list = []
+            name_list = []
+            sum_params_list = []
             for idx in range(num_dfe - 1, -1, -1):
+                dfe_params = dfe_params_list[idx]
                 didx = idx + 3
-                term_dict = {'inp': 'inp_d<%d>' % didx, 'inn': 'inn_d<%d>' % didx,
-                             'outp_l': 'outp_d<%d>' % didx, 'outn_l': 'outn_d<%d>' % didx,
-                             'casc<1:0>': 'sgnn<%d>,sgnp<%d>' % (didx, didx),
-                             'biasn_s': 'biasn_s<%d>' % didx, }
+                dfe_suf = '<%d>' % didx
+                load_suf = '<%d>' % load_idx
+                load_idx += 1
+                if dfe_params.get('flip_sign', False):
+                    outp_s = 'iin' + load_suf
+                    outn_s = 'iip' + load_suf
+                else:
+                    outp_s = 'iip' + load_suf
+                    outn_s = 'iin' + load_suf
+                term_dict = {'inp': 'inp_d' + dfe_suf, 'inn': 'inn_d' + dfe_suf,
+                             'outp_l': 'outp_d' + dfe_suf, 'outn_l': 'outn_d' + dfe_suf,
+                             'casc<1:0>': 'sgnn' + dfe_suf + ',sgnp' + dfe_suf,
+                             'biasn_s': 'biasn_s' + dfe_suf, 'outp_s': outp_s, 'outn_s': outn_s}
                 term_list.append(term_dict)
                 name_list.append('XDFE%d' % didx)
+                sum_params_list.append(dfe_params['sum_params'])
+                load_params_list.append(dfe_params['load_params'])
             # array instance, and design
             self.array_instance('XDFE', name_list, term_list=term_list)
-            for idx, dfe_params in enumerate(dfe_params_list):
-                self.instances['XDFE'][num_dfe - 1 - idx].design(**dfe_params)
+            for idx, sum_params in enumerate(sum_params_list):
+                self.instances['XDFE'][idx].design(**sum_params)
 
         # design last cell
         self.reconnect_instance_terminal('XLAST', 'inp', 'inp_d<2>')
         self.reconnect_instance_terminal('XLAST', 'inn', 'inn_d<2>')
         self.reconnect_instance_terminal('XLAST', 'biasn', 'biasn_s<2>')
         self.reconnect_instance_terminal('XLAST', 'casc<1:0>', 'sgnn<2>,sgnp<2>')
-        self.instances['XLAST'].design(**last_params)
+        load_suf = '<%d>' % load_idx
+        load_idx += 1
+        if last_params.get('flip_sign', False):
+            outp_s = 'iin' + load_suf
+            outn_s = 'iip' + load_suf
+        else:
+            outp_s = 'iip' + load_suf
+            outn_s = 'iin' + load_suf
+        self.reconnect_instance_terminal('XLAST', 'outp', outp_s)
+        self.reconnect_instance_terminal('XLAST', 'outn', outn_s)
+        self.instances['XLAST'].design(**last_params['sum_params'])
+        load_params_list.append(last_params['load_params'])
+
+        # design load
+
 
         # remove pins if not needed
         if 'pulse' not in self.instances['XFFE'][0].master.pin_list:
