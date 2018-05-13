551afba823d51fa9835b65dcf933b269ba022a12
Eric Chang <pkerichang@gmail.com>
2018-05-13 01:32:05 -0700

    1. update Tap1Column test script.  DRC and LVS clean.

--- scripts_test/qdr_hybrid/tap1_column.py
+++ scripts_test/qdr_hybrid/tap1_column.py
@@ -21,4 +21,4 @@ if __name__ == '__main__':
         bprj = local_dict['bprj']
 
     bprj.generate_cell(block_specs, Tap1Column, debug=True)
-    # bprj.generate_cell(block_specs, Tap1Column, gen_sch=True, run_lvs=False)
+    # bprj.generate_cell(block_specs, Tap1Column, gen_sch=True, debug=True)
