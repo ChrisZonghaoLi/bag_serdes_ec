d376d1a9a9a30d8228abf3ad6c5b26340002c5cc
Eric Chang <pkerichang@gmail.com>
2018-05-14 21:35:30 -0700

    1. update datapath to not fill layer 5.

--- serdes_ec/layout/qdr_hybrid/datapath.py
+++ serdes_ec/layout/qdr_hybrid/datapath.py
@@ -213,8 +213,7 @@ class RXDatapath(TemplateBase):
             self.do_max_space_fill(layer, bound_box=box2)
 
         self.fill_box = tapx_box.merge(samp_box)
-        for layer in range(hm_layer, hm_layer + 2):
-            self.do_max_space_fill(layer, self.fill_box)
+        self.do_max_space_fill(hm_layer, self.fill_box)
 
     def _connect_supplies(self, tapx, tap1, offset, offlev, samp, show_pins):
         fill_w = self.params['fill_w']
