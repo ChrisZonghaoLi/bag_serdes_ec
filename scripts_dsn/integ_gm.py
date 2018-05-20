5d8009f959bb697b0386df241bfd784ded7b024c
Eric Chang <pkerichang@gmail.com>
2018-05-20 08:02:14 -0700

    working on design script.

--- scripts_dsn/integ_gm.py
+++ scripts_dsn/integ_gm.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
+import matplotlib.pyplot as plt
+# noinspection PyUnresolvedReferences
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib import ticker
+
 from bag.core import BagProject
-from bag.io.sim_data import load_sim_results, save_sim_results
+from bag.io.sim_data import load_sim_results, save_sim_results, load_sim_file
 
 
-def simulate(prj):
+def simulate(prj, save_fname):
     tb_lib = 'bag_serdes_testbenches_ec'
     tb_cell = 'gm_char_dc'
     dut_lib = 'CHAR_INTEG_AMP_FG4'
     dut_cell = 'INTEG_AMP'
     impl_lib = 'CHAR_INTEG_AMP_FG4_TB'
     impl_cell = tb_cell
-    save_fname = 'blocks_ec_tsmcN16/data/gm_char_dc/linearity_fg4.hdf5'
     env_list = ['tt', 'ff_hot', 'ss_cold']
     sim_view = 'av_extracted'
 
@@ -25,7 +32,7 @@ def simulate(prj):
         outcm=0.8,
         vb0=-vck_amp,
         vb1=vdd,
-        num=41,
+        num=40,
     )
     vstar_step = vstar_max * 2 / (vstar_num - 1)
 
@@ -56,6 +63,52 @@ def simulate(prj):
     save_sim_results(data, save_fname)
 
 
+def get_data(save_fname):
+    result = load_sim_file(save_fname)
+    return result
+
+
+def plot_data_2d(result, name, sim_env=None):
+    """Get interpolation function and plot/query."""
+
+    swp_pars = result['sweep_params'][name]
+    data = result[name]
+
+    if sim_env is not None:
+        corners = result['corner']
+        corner_idx = swp_pars.index('corner')
+        env_idx = np.argwhere(corners == sim_env)[0][0]
+        data = np.take(data, env_idx, axis=corner_idx)
+        swp_pars = swp_pars[:corner_idx] + swp_pars[corner_idx + 1:]
+
+    xvec = result[swp_pars[0]]
+    yvec = result[swp_pars[1]]
+
+    xmat, ymat = np.meshgrid(xvec, yvec, indexing='ij', copy=False)
+
+    formatter = ticker.ScalarFormatter(useMathText=True)
+    formatter.set_scientific(True)
+    formatter.set_powerlimits((-2, 3))
+
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(xmat, ymat, data, rstride=1, cstride=1, linewidth=0, cmap=cm.cubehelix)
+    ax.set_xlabel(swp_pars[0])
+    ax.set_ylabel(swp_pars[1])
+    ax.set_zlabel(name)
+    ax.w_zaxis.set_major_formatter(formatter)
+
+    plt.show()
+
+
+def run_main(prj):
+    save_fname = 'blocks_ec_tsmcN16/data/gm_char_dc/linearity.hdf5'
+
+    # simulate(prj, save_fname)
+    data = get_data(save_fname)
+    plot_data_2d(data, 'ioutp', sim_env='tt')
+
+
 if __name__ == '__main__':
     local_dict = locals()
     if 'bprj' not in local_dict:
@@ -66,4 +119,4 @@ if __name__ == '__main__':
         print('loading BAG project')
         bprj = local_dict['bprj']
 
-    simulate(bprj)
+    run_main(bprj)
