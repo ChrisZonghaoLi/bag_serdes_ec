3a01d81c2c1529b3cd1ee736bcd360be04dc3559
Eric Chang <pkerichang@gmail.com>
2018-05-24 11:36:57 -0700

    1. update buffer array to be able to generate wires with different    widths and spacing.

--- serdes_ec/layout/digital/buffer.py
+++ serdes_ec/layout/digital/buffer.py
@@ -4,7 +4,7 @@
 
 from typing import TYPE_CHECKING, Dict, Any, Set, List
 
-from bag.layout.routing.base import TrackID, WireArray
+from bag.layout.routing.base import TrackID, WireArray, TrackManager
 
 from digital_ec.layout.stdcells.core import StdDigitalTemplate
 from digital_ec.layout.stdcells.inv import InvChain
@@ -58,6 +58,7 @@ class BufferRow(StdDigitalTemplate):
             seg_list='number of segments for each inverter in the buffer.',
             tr_widths='Track width dictionary.',
             tr_spaces='Track spacing dictionary.',
+            in_idx0='input track index.',
             out_delta='output track spacing from middle wire.',
             ncol_min='Minimum number of columns.',
             wp_list='list of PMOS widths.',
@@ -70,6 +71,7 @@ class BufferRow(StdDigitalTemplate):
     def get_default_param_values(cls):
         # type: () -> Dict[str, Any]
         return dict(
+            in_idx0=None,
             out_delta=1,
             ncol_min=0,
             wp_list=None,
@@ -81,8 +83,11 @@ class BufferRow(StdDigitalTemplate):
     def draw_layout(self):
         nbuf = self.params['nbuf']
         seg_list = self.params['seg_list']
+        tr_widths = self.params['tr_widths']
+        tr_spaces = self.params['tr_spaces']
         out_delta = self.params['out_delta']
         ncol_min = self.params['ncol_min']
+        in_idx0 = self.params['in_idx0']
         show_pins = self.params['show_pins']
 
         base_params = self.params.copy()
@@ -113,16 +118,22 @@ class BufferRow(StdDigitalTemplate):
 
         # draw instances, and export ports
         hm_layer = vm_layer + 1
+        tr_manager = TrackManager(self.grid, tr_widths, tr_spaces, half_space=True)
+        hm_tr_w = tr_manager.get_width(hm_layer, 'buf_sig')
+        hm_tr_sp = tr_manager.get_space(hm_layer, ('buf_sig', 'buf_sig'))
+        hm_pitch = hm_tr_w + hm_tr_sp
         num_h_tr2 = self.get_num_x_tracks(hm_layer, half_int=True)
-        in_idx0 = ((num_h_tr2 - 2 * nbuf) // 2) / 2
+        if in_idx0 is None:
+            # TODO: keep old behavior for now to not disrupt tapeout
+            in_idx0 = ((num_h_tr2 - 2 * nbuf) // 2) / 2
         for idx in range(nbuf):
             cidx = tap_ncol + self._blk_sp + idx * buf_ncol
             cur_inst = self.add_digital_block(master, (cidx, 0))
             cur_mid = cur_inst.translate_master_track(vm_layer, mid_tidx)
+            cur_tid = TrackID(hm_layer, in_idx0 + idx * hm_pitch, width=hm_tr_w)
             cur_in = self.connect_to_tracks(cur_inst.get_pin('in'), TrackID(vm_layer, cur_mid - 1),
                                             min_len_mode=0)
-            cur_in = self.connect_to_tracks(cur_in, TrackID(hm_layer, in_idx0 + idx),
-                                            min_len_mode=None)
+            cur_in = self.connect_to_tracks(cur_in, cur_tid, min_len_mode=None)
             self.add_pin('in<%d>' % idx, cur_in, show=show_pins)
             self.add_pin('out<%d>' % idx, cur_inst.get_pin('out'), show=show_pins)
         self.fill_space()
@@ -187,6 +198,8 @@ class BufferArray(StdDigitalTemplate):
             seg_list='number of segments for each inverter in the buffer.',
             tr_widths='Track width dictionary.',
             tr_spaces='Track spacing dictionary.',
+            in_idx0='input track index.',
+            in_pitch='input pitch between rows.',
             out_vertical='True if the output is vertical.',
             ncol_min='Minimum number of columns.',
             wp_list='list of PMOS widths.',
@@ -199,6 +212,8 @@ class BufferArray(StdDigitalTemplate):
     def get_default_param_values(cls):
         # type: () -> Dict[str, Any]
         return dict(
+            in_idx0=None,
+            in_pitch=None,
             out_vertical=True,
             ncol_min=0,
             wp_list=None,
@@ -210,6 +225,8 @@ class BufferArray(StdDigitalTemplate):
     def draw_layout(self):
         nbuf_list = self.params['nbuf_list']
         seg_list = self.params['seg_list']
+        in_idx0 = self.params['in_idx0']
+        in_pitch = self.params['in_pitch']
         out_vertical = self.params['out_vertical']
         ncol_min = self.params['ncol_min']
         row_layout_info = self.params['row_layout_info']
@@ -220,8 +237,12 @@ class BufferArray(StdDigitalTemplate):
         tech_info = self.grid.tech_info
         lch_unit = self.lch_unit
         ncol = 0
+        nbuf_tot = 0
+        nbuf_max = 0
         for nbuf in nbuf_list:
             ncol = max(ncol, BufferRow.compute_num_cols(tech_info, lch_unit, nbuf, seg_list))
+            nbuf_tot += nbuf
+            nbuf_max = max(nbuf_max, nbuf)
         ncol = max(ncol, ncol_min)
 
         # setup floorplan and add instances
@@ -233,22 +254,26 @@ class BufferArray(StdDigitalTemplate):
         vss_list = []
         buf_params = None
         idx0 = 0
-        nbuf_tot = 0
         vm_layer = self.conn_layer + 2
         hm_layer = vm_layer + 1
         delta_le = self.grid.get_line_end_space_tracks(hm_layer, vm_layer, 1, half_space=True)
+        ntr_hm = 0
         for ridx, nbuf in enumerate(nbuf_list):
-            nbuf_tot += nbuf
             params['nbuf'] = nbuf
             if out_vertical:
                 params['out_delta'] = ridx + 1
             else:
                 params['out_delta'] = delta_le
+            if ridx % 2 == 1 and in_pitch is not None:
+                params['in_idx0'] = ntr_hm - 1 - (in_idx0 + (nbuf_max - 1) * in_pitch)
+            else:
+                params['in_idx0'] = in_idx0
             master = self.new_template(params=params, temp_cls=BufferRow)
             if row_layout_info is None:
                 buf_params = master.sch_params['buf_params']
                 params['row_layout_info'] = row_layout_info = master.row_layout_info
                 self.initialize(row_layout_info, nrow, ncol, draw_boundaries=True, end_mode=15)
+                ntr_hm = self.get_num_x_tracks(hm_layer)
             inst = self.add_digital_block(master, (0, ridx))
             for idx in range(nbuf):
                 in_pin = inst.get_pin('in<%d>' % idx)
