6f1d85b0755f3e8796b0eaf8f8eac977224737ee
Eric Chang <pkerichang@gmail.com>
2018-06-01 19:51:53 -0700

    1. fix bug in differential high pass filter.

--- scripts_test/qdr_hybrid/datapath.py
+++ scripts_test/qdr_hybrid/datapath.py
@@ -20,5 +20,5 @@ if __name__ == '__main__':
         print('loading BAG project')
         bprj = local_dict['bprj']
 
-    bprj.generate_cell(block_specs, RXDatapath, debug=True)
-    # bprj.generate_cell(block_specs, RXDatapath, gen_sch=True, debug=True)
+    # bprj.generate_cell(block_specs, RXDatapath, debug=True)
+    bprj.generate_cell(block_specs, RXDatapath, gen_sch=True, debug=True)
